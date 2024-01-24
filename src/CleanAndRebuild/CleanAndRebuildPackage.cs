5ce83795969a54efeacda66dc591214ac22496db
Chris Pulman <chris.pulman@yahoo.com>
2024-01-24 23:14:10 +0000

    Add Initial code

---
+++ src/CleanAndRebuild/CleanAndRebuildPackage.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Chris Pulman. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace CPCleanAndRebuild
+{
+    /// <summary>
+    /// CleanAndRebuildPackage.
+    /// </summary>
+    /// <seealso cref="Microsoft.VisualStudio.Shell.AsyncPackage" />
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(Vsix.Id)]
+    [ProvideOptionPage(typeof(Options), "Environment", Vsix.Name, 101, 102, true, new string[] { }, ProvidesLocalizedCategoryName = false)]
+    public sealed class CleanAndRebuildPackage : AsyncPackage
+    {
+        /// <summary>
+        /// The async initialization portion of the package initialization process. This method is invoked from a background thread.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">Show the update progress.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await CleanAndRebuildCommand.InitializeAsync(this);
+        }
+    }
+}
---
+++ src/CleanAndRebuild/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Chris Pulman. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+using CPCleanAndRebuild;
+
+[assembly: AssemblyTitle(Vsix.Name)]
+[assembly: AssemblyDescription(Vsix.Description)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(Vsix.Author)]
+[assembly: AssemblyProduct(Vsix.Name)]
+[assembly: AssemblyCopyright(Vsix.Author)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture(Vsix.Language)]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion(Vsix.Version)]
+[assembly: AssemblyFileVersion(Vsix.Version)]
